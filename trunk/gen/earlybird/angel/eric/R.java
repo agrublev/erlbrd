ade14254cd19408027c6913506799bf344725f7e
agrublev <agrublev@9d1b12b6-6a05-4270-fed5-e334e25b2e1b>
2010-09-22 21:43:12 +0000

    Made the alarm display what time you want to be awakened and also play a sound :) YAY I ROC!

--- trunk/gen/earlybird/angel/eric/R.java
+++ trunk/gen/earlybird/angel/eric/R.java
@@ -13,6 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int LinearLayout01=0x7f060003;
+        public static final int alarmTitle=0x7f060000;
+        public static final int calendarList=0x7f060006;
+        public static final int calendarTitle=0x7f060005;
+        public static final int pickTime=0x7f060002;
+        public static final int startAlarm=0x7f060004;
+        public static final int textViewTips=0x7f06000c;
+        public static final int timeDisplay=0x7f060001;
+        public static final int tipsTitle=0x7f06000b;
+        public static final int viewCalendar=0x7f060008;
+        public static final int viewSettings=0x7f060007;
+        public static final int viewTestAlarm=0x7f060009;
+        public static final int viewTipsAndTricks=0x7f06000a;
+    }
     public static final class layout {
         public static final int alarm=0x7f030000;
         public static final int calendar=0x7f030001;
@@ -22,8 +37,12 @@ public final class R {
         public static final int settings=0x7f030005;
         public static final int tips=0x7f030006;
     }
+    public static final class raw {
+        public static final int alarm_01=0x7f040000;
+        public static final int alarm_02=0x7f040001;
+    }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
--- trunk/src/earlybird/angel/eric/alarm.java
+++ trunk/src/earlybird/angel/eric/alarm.java
@@ -1,14 +1,94 @@
 package earlybird.angel.eric;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.Calendar;
 
-public class alarm extends Activity {
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class alarm extends Activity implements OnClickListener {
+	private TextView mTimeDisplay;
+    private Button mPickTime;
+
+    private int mHour;
+    private int mMinute;
+	private Button startAlarm;
+
+    static final int TIME_DIALOG_ID = 0;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm); 
+        
+     // capture our View elements
+        mTimeDisplay = (TextView) findViewById(R.id.timeDisplay);
+        mPickTime = (Button) findViewById(R.id.pickTime);
+        startAlarm = (Button) findViewById(R.id.startAlarm);
+        startAlarm.setOnClickListener(this);
+
+        // add a click listener to the button
+        mPickTime.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(TIME_DIALOG_ID);
+            }
+        });
+
+        // get the current time
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        // display the current date
+        updateDisplay();
+
     }
+    public void onClick(View v) {
+    	if (v == startAlarm) {
+    		Toast.makeText(this, mTimeDisplay.getText(), Toast.LENGTH_LONG).show();
+    	    MediaPlayer mp = MediaPlayer.create(alarm.this, R.raw.alarm_01);
+    	    mp.start();
+    	}
+    }
+ // updates the time we display in the TextView
+    private void updateDisplay() {
+        mTimeDisplay.setText(
+            new StringBuilder()
+                    .append(pad(mHour)).append(":")
+                    .append(pad(mMinute)));
+    }
+
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
+ // the callback received when the user "sets" the time in the dialog
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+        new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                mHour = hourOfDay;
+                mMinute = minute;
+                updateDisplay();
+            }
+    };
     
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        case TIME_DIALOG_ID:
+            return new TimePickerDialog(this,
+                    mTimeSetListener, mHour, mMinute, false);
+        }
+        return null;
+    }
 }
