a59bc52b1a20bddce295dcf7da83eebcc2da9c61
agrublev <agrublev@9d1b12b6-6a05-4270-fed5-e334e25b2e1b>
2010-09-23 16:03:12 +0000

    new

--- trunk/src/earlybird/angel/eric/settings.java
+++ trunk/src/earlybird/angel/eric/settings.java
@@ -2,6 +2,8 @@ package earlybird.angel.eric;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class settings extends Activity {
 	/** Called when the activity is first created. */
@@ -9,6 +11,12 @@ public class settings extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings); 
+        Spinner s = (Spinner) findViewById(R.id.Spinner01);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this, R.array.options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
     }
     
 }
