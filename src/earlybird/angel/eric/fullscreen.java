e41ed847c8435d1140ceb854e44eccba1b3562e6
agrublev <agrublev@9d1b12b6-6a05-4270-fed5-e334e25b2e1b>
2010-09-26 22:31:49 +0000

    new shait

---
+++ src/earlybird/angel/eric/fullscreen.java
@@ -0,0 +1,14 @@
+package earlybird.angel.eric;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class fullscreen extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fullscreen); 
+	}
+	
+}
