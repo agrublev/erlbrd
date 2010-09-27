e14bfc4d3dda66eb26101f0d918df57e69e57d14
agrublev <agrublev@9d1b12b6-6a05-4270-fed5-e334e25b2e1b>
2010-09-27 19:31:41 +0000

    new shait

--- trunk/src/earlybird/angel/eric/test.java
+++ trunk/src/earlybird/angel/eric/test.java
@@ -2,11 +2,10 @@ package earlybird.angel.eric;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.hardware.SensorEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -16,7 +15,12 @@ public class test extends Activity implements OnClickListener, SensorListener {
 	/** Called when the activity is first created. */
 	private static final String TAG = "Sensors";
     private SensorManager mSensorManager;
-    float x, y, z = 0;
+    /*	
+     * s1-s5 are the values for 5 different readings, d1-d5 are the diviations or deltas, and c is the combined, td is total delta avarage of 5
+     * max and min are the highest and lowest deltas observed for calibration purposes
+     * */
+    float x, y, z, s1, s2, s3, s4, s5, s, d1, d2, d3, d4, d5, c, td, max, min = 0;
+    private soundmanager mSoundManager;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,9 @@ public class test extends Activity implements OnClickListener, SensorListener {
         
         setContentView(R.layout.calibration); 
         
-        
+        mSoundManager = new soundmanager();
+        mSoundManager.initSounds(getBaseContext());
+        mSoundManager.addSound(1, R.raw.move);
         
         IntentButton backButton = (IntentButton) findViewById(R.id.backButton);
         backButton.intent = new Intent(this, main.class);
@@ -40,22 +46,71 @@ public class test extends Activity implements OnClickListener, SensorListener {
 	public void onAccuracyChanged(int arg0, int arg1) {
 		// TODO Auto-generated method stub
 		
-	}
+	}/*
 	public void onSensorChanged(SensorEvent event) {
         
         ((TextView) findViewById(R.id.x_label)).setText(Float.toString(event.values[0]));
         ((TextView) findViewById(R.id.y_label)).setText(Float.toString(event.values[1]));
         ((TextView) findViewById(R.id.z_label)).setText(Float.toString(event.values[2]));
 
-	}
-
+	}*/
+	
 	public void onSensorChanged(int sensor, float[] values) {
 		synchronized (this) {
+			x = Math.abs(values[0]);
+			y = Math.abs(values[1]);
+			z = Math.abs(values[2]);
+			c = x + y + z;
+			if (s == 0) {
+				s = c;
+			} else {
+				if (s1 == 0) {
+					s1 = c;
+					d1 = s - s1;
+					d1 = Math.abs(d1);
+				} else if (s2 == 0) {
+					s2 = c;
+					d2 = s - s2;
+					d2 = Math.abs(d2);
+				} else if (s3 == 0) {
+					s3 = c;
+					d3 = s - s3;
+					d3 = Math.abs(d3);
+				} else if (s4 == 0) {
+					s4 = c;
+					d4 = s - s4;
+					d4 = Math.abs(d4);
+				} else if (s5 == 0) {
+					s5 = c;
+					d4 = s - s5;
+					d5 = Math.abs(d5);
+				} else {
+					td = d1 + d2 + d3 + d4 + d5;
+					td = td/5;
+					if (max < td) {
+						max = td;
+						((TextView) findViewById(R.id.max_label)).setText(Float.toString(max));
+					}
+					if (min == 0) {
+						min = td;
+					} else if (min > td) {
+						min = td;
+						((TextView) findViewById(R.id.min_label)).setText(Float.toString(min));
+					}
+					((TextView) findViewById(R.id.d_label)).setText(Float.toString(td));
+					if (td >= 5) {
+						mSoundManager.playSound(1);
+					}
+					s = s1 = s2 = s3 = s4 = s5 = 0;
+				}
+			}
+			
+			
 	        ((TextView) findViewById(R.id.x_label)).setText(Float.toString(values[0]));
 	        ((TextView) findViewById(R.id.y_label)).setText(Float.toString(values[1]));
 	        ((TextView) findViewById(R.id.z_label)).setText(Float.toString(values[2]));
 		}
-	}
+	} 
 	  @Override
 	    protected void onResume() {
 	    super.onResume();
