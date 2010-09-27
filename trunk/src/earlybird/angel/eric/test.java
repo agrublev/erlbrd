c86b382360b222d7efd2d6f030dbcb596f34c0df
agrublev <agrublev@9d1b12b6-6a05-4270-fed5-e334e25b2e1b>
2010-09-27 16:53:41 +0000

    getting fed the REAL acceleration values

--- trunk/src/earlybird/angel/eric/test.java
+++ trunk/src/earlybird/angel/eric/test.java
@@ -1,18 +1,31 @@
 package earlybird.angel.eric;
 
-import elements.*;
 import android.app.Activity;
-import android.content.*;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.*;
-import android.view.View.*;
+import android.hardware.SensorEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import elements.IntentButton;
 
-public class test extends Activity implements OnClickListener {
+public class test extends Activity implements OnClickListener, SensorListener {
 	/** Called when the activity is first created. */
+	private static final String TAG = "Sensors";
+    private SensorManager mSensorManager;
+    float x, y, z = 0;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        
         setContentView(R.layout.calibration); 
+        
+        
         
         IntentButton backButton = (IntentButton) findViewById(R.id.backButton);
         backButton.intent = new Intent(this, main.class);
@@ -23,4 +36,31 @@ public class test extends Activity implements OnClickListener {
     	IntentButton b = (IntentButton) v;
     	startActivity(b.intent);
     }
+
+	public void onAccuracyChanged(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void onSensorChanged(SensorEvent event) {
+        
+        ((TextView) findViewById(R.id.x_label)).setText(Float.toString(event.values[0]));
+        ((TextView) findViewById(R.id.y_label)).setText(Float.toString(event.values[1]));
+        ((TextView) findViewById(R.id.z_label)).setText(Float.toString(event.values[2]));
+
+	}
+
+	public void onSensorChanged(int sensor, float[] values) {
+		synchronized (this) {
+	        ((TextView) findViewById(R.id.x_label)).setText(Float.toString(values[0]));
+	        ((TextView) findViewById(R.id.y_label)).setText(Float.toString(values[1]));
+	        ((TextView) findViewById(R.id.z_label)).setText(Float.toString(values[2]));
+		}
+	}
+	  @Override
+	    protected void onResume() {
+	    super.onResume();
+	    // Register this class as a listener for the accelerometer sensor
+	    mSensorManager.registerListener(this, SensorManager.SENSOR_ACCELEROMETER);
+	  }
+
 }
